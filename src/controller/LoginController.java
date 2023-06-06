7a3da8d00c8b6614977ffde63981b23a2fb47e03
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-06-06 16:01:23 +0530

    login form part1

---
+++ src/controller/LoginController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+    public AnchorPane mainPane;
+    public JFXTextField txtuserName;
+    public JFXTextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}
