9ee9beb5442c8cd9b0fff249abf4294695052f77
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-06-06 19:55:58 +0530

    create the client Form Part1

---
+++ src/controller/ChatFormController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class ChatFormController {
+    public JFXTextField txtClientMessage;
+    public VBox chatListContext;
+    public Label lblUserName;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+
+    }
+}
